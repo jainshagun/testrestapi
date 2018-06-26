b8c12023df02ffb3325808bf97b8229897309c23
jainshagun <40594574+jainshagun@users.noreply.github.com>
2018-06-26 20:14:23 +0100

    Update AppTest.java

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -3,6 +3,7 @@ package com.mycompany.app;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+import static org.junit.Assert.*;
 
 public class AppTest {
 	
