0e8b35c274f15c3de4d10cf0d8acb7c9274032b8
jainshagun <40594574+jainshagun@users.noreply.github.com>
2018-06-27 10:09:22 +0100

    Update AppTest.java

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -6,7 +6,7 @@ import static org.junit.Assert.assertEquals;
 public class AppTest {
 	
    String message = "Hello World";	
-   App messageUtil = new App(message);
+   //App messageUtil = new App(message);
 
    @Test
    public void testPrintMessage() {
